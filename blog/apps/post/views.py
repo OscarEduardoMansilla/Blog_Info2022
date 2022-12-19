4a0140446256c4a15f94ca0717d408c9cb16b94a
Mariana Mansilla <osedmansi@gmail.comgit config --global user.name OscarEduardoMansilla>
2022-12-19 11:29:32 -0300

    primer commit

--- blog/apps/post/views.py
+++ blog/apps/post/views.py
@@ -1,3 +1,72 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.views.generic import View
+from django.urls import reverse_lazy
+from .models import *
+from .forms import PostForm
 
 # Create your views here.
+
+#Vista para crear un posteo
+def crearPost(request):
+    if request.method == 'POST':
+        post_form = PostForm(request.POST or None, request.FILES or None)
+        if post_form.is_valid():
+           post_form.save()
+           return redirect('crear_post')
+    else:
+            post_form = PostForm()
+    return render (request, 'post/crear_post.html', {'post_form': post_form})   
+
+class MostrarPost(View):
+    template_name = "post/posteos.html"
+
+    def get(self, request):
+        posteos = Post.objects.all()
+        categorias = Categoria.objects.all()
+        context = {
+            "posteos": posteos,
+            "categorias": categorias
+        }
+        return render(request, self.template_name, context) 
+
+    def post(self, request):   
+        categorias = Categoria.objects.all()
+        cate = request.POST.get("categoria", None)
+        fecha = request.POST.get('fecha', None)
+        if cate and fecha:
+            posteos = Post.objects.filter (categoria_nombre = cate, fecha_creacion = fecha)
+        elif cate:
+           posteos = Post.objects.filter(categoria_nombre = cate)
+        elif fecha:
+           posteos = Post.objects.filter(fecha_creacion = fecha)
+
+        context = {
+                    "posteos": posteos,
+                    "categorias": categorias
+        }
+        return render(request, self.template_name, context) 
+
+
+
+def leerPost(request, id):
+    if request.method=='GET':
+        post = Post.objects.get(id=id)
+        comentarios = Comentario.objects.filter(posteo=id)
+        context = {
+            'post': post,
+            'comentarios':comentarios
+        }
+    return render(request, 'post/posteo.html', context)
+
+
+@login_required
+def comentar_Post(request):
+
+    com = request.POST.get('comentario',None)
+    usu = request.user
+    noti = request.POST.get('id_post', None)
+    post = Post.objects.get(id = noti) 
+    Comentario.objects.create(usuario = usu, posteo = post, texto = com)
+
+    return redirect(reverse_lazy('posteo', kwargs={'id': noti}))
